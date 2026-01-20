5cb4bce51f60376153672b6a855e567dfbe4d4af
Taofeeqfillyou <Taofeeq@fillyou.in>
2026-01-20 12:25:41 +0100

    fix

--- main/settings.py
+++ main/settings.py
@@ -127,7 +127,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=config('DATABASE_URL'),
         conn_max_age=600,
         conn_health_checks=True,
     )
